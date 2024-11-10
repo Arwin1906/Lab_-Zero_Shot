369ece54bb897dc7da5d7d126ea5344ba0c5ea95
arwin <arwin1906@gmail.com>
2024-11-10 18:27:36 +0100

    added utils

---
+++ Arwin/src/plot_utils.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
+import torch
+
+@torch.no_grad()
+def plot_progress(data_loader, model):
+
+    fig, ax = plt.subplots(4, 3, figsize=(15,15))
+    X = np.linspace(0, 1, 128)
+
+    matplotlib.use('Agg')  # Use Agg backend for rendering images without displaying them
+
+    for i, (function_values, observations) in enumerate(data_loader):
+        
+        values, times = observations
+        prediction = model(values, times)
+        ground_truth = function_values
+
+        for j in range(len(ground_truth[0])):
+            ax[i, 0].plot(X, ground_truth[j].detach().cpu().numpy())
+            ax[i, 0].plot(X, prediction[j].detach().cpu().numpy())
+
+            ax[i, 1].plot(X, ground_truth[j+1].detach().cpu().numpy())
+            ax[i, 1].plot(X, prediction[j+1].detach().cpu().numpy())
+
+            ax[i, 2].plot(X, ground_truth[j+2].detach().cpu().numpy())
+            ax[i, 2].plot(X, prediction[j+2].detach().cpu().numpy())
+
+            break
+            
+        if i == 3:
+            break
+
+    # Create custom legend handles
+    ground_truth_handle = mlines.Line2D([], [], color='blue', label='Ground truth')
+    prediction_handle = mlines.Line2D([], [], color='orange', label='Prediction')
+
+    # Add the custom legend to the figure
+    fig.legend(handles=[ground_truth_handle, prediction_handle], loc='upper right',fontsize=20)
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+
+    return fig
+
+def smooth(f, K=5):
+    """ Smoothing a function using a low-pass filter (mean) of size K """
+    kernel = np.ones(K) / K
+    f = np.concatenate([f[:int(K//2)], f, f[int(-K//2):]])  # to account for boundaries
+    smooth_f = np.convolve(f, kernel, mode="same")
+    smooth_f = smooth_f[K//2: -K//2]  # removing boundary-fixes
+    return smooth_f
+
+def visualize_progress(train_loss, val_loss, start=0):
+    """ Visualizing loss and accuracy """
+    fig, ax = plt.subplots(1,3)
+    fig.set_size_inches(24,5)
+
+    smooth_train = smooth(train_loss, 31)
+    ax[0].plot(train_loss, c="blue", label="Loss", linewidth=3, alpha=0.5)
+    ax[0].plot(smooth_train, c="red", label="Smoothed Loss", linewidth=3, alpha=1)
+    ax[0].legend(loc="best")
+    ax[0].set_xlabel("Iteration")
+    ax[0].set_ylabel("CE Loss")
+    ax[0].set_yscale("linear")
+    ax[0].set_title("Training Progress (linear)")
+    
+    ax[1].plot(train_loss, c="blue", label="Loss", linewidth=3, alpha=0.5)
+    ax[1].plot(smooth_train, c="red", label="Smoothed Loss", linewidth=3, alpha=1)
+    ax[1].legend(loc="best")
+    ax[1].set_xlabel("Iteration")
+    ax[1].set_ylabel("CE Loss")
+    ax[1].set_yscale("log")
+    ax[1].set_title("Training Progress (log)")
+
+    smooth_val = smooth(val_loss, 31)
+    N_ITERS = len(val_loss)
+    ax[2].plot(np.arange(start, N_ITERS)+start, val_loss[start:], c="blue", label="Loss", linewidth=3, alpha=0.5)
+    ax[2].plot(np.arange(start, N_ITERS)+start, smooth_val[start:], c="red", label="Smoothed Loss", linewidth=3, alpha=1)
+    ax[2].legend(loc="best")
+    ax[2].set_xlabel("Iteration")
+    ax[2].set_ylabel("CE Loss")
+    ax[2].set_yscale("log")
+    ax[2].set_title(f"Valid Progress")
+
+    return
---
+++ Arwin/src/utils.py
@@ -0,0 +1,37 @@
+import os
+import torch
+
+def save_model(model, optimizers, epoch, stats, modelname):
+    """ Saving model checkpoint """
+    
+    if(not os.path.exists(f"./Arwin/checkpoints/{modelname}")):
+        os.makedirs(f"./Arwin/checkpoints/{modelname}")
+    savepath = f"./Arwin/checkpoints/{modelname}/checkpoint_epoch_{epoch}_{modelname}.pth"
+
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optim_branch_state_dict': optimizers["branch"].state_dict(),
+        'optim_trunk_state_dict': optimizers["trunk"].state_dict(),
+        'stats': stats
+    }, savepath)
+    return
+
+
+def load_model(model, optimizers, savepath):
+    """ Loading pretrained checkpoint """
+    
+    checkpoint = torch.load(savepath, map_location="cpu")
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizers["branch"].load_state_dict(checkpoint['optim_branch_state_dict'])
+    optimizers["trunk"].load_state_dict(checkpoint['optim_trunk_state_dict'])
+    epoch = checkpoint["epoch"]
+    stats = checkpoint["stats"]
+    
+    return model, optimizers, epoch, stats
+
+
+def count_model_params(model):
+    """ Counting the number of learnable parameters in a nn.Module """
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return num_params
