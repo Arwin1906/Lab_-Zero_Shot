e2f0698eae95eeb77cc3c09238dab3bb35b3a83f
arwin <arwin1906@gmail.com>
2024-11-14 23:43:18 +0100

    adjusted for new model

--- Arwin/model/trainer.py
+++ Arwin/model/trainer.py
@@ -1,4 +1,6 @@
 import torch
+import math
+import optuna
 import numpy as np
 from tqdm import tqdm
 import torch.nn.functional as F
@@ -45,12 +47,37 @@ class CombinedScheduler(torch.optim.lr_scheduler._LRScheduler):
             self.main_scheduler.step(self.last_epoch - self.warmup_epochs)
 
         self.last_epoch += 1
+
+class InverseSquareRootLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, warmup_steps, init_lr, min_lr=1e-9, last_epoch=-1):
+        self.warmup_steps = warmup_steps
+        self.init_lr = init_lr
+        self.min_lr = min_lr
+        super(InverseSquareRootLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        step = self.last_epoch + 1
+        if step < self.warmup_steps:
+            # Linear warmup
+            lr = self.init_lr * (step / self.warmup_steps)
+        else:
+            # Inverse square root decay
+            lr = self.init_lr * math.sqrt(self.warmup_steps / step)
+
+        # Ensure learning rate doesn't go below minimum
+        lr = max(lr, self.min_lr)
+
+        return [lr for _ in self.base_lrs]
     
 class Trainer:
     """
     Class for training and validating a transformer model
     """
-    def __init__(self, model, criterion, train_loader, valid_loader, modelname, n_iters=2, writer=None, inital_iter=0, schedule=True):
+    def __init__(
+            self, model, criterion, train_loader, valid_loader, modelname,
+            epochs=2, writer=None, inital_epoch=0, schedule=True,
+            optuna=False, optuna_trial=None
+        ):
         """ Trainer initializer """
         self.model = model
         self.writer = writer
@@ -58,27 +85,33 @@ class Trainer:
         self.train_loader = train_loader
         self.valid_loader = valid_loader
         self.modelname = modelname
-        self.inital_iter= inital_iter
-        self.n_iters = int(n_iters)
+        self.inital_epoch= inital_epoch
+        self.epochs = int(epochs)
         self.schedule = schedule
-
-        lr_branch = 1e-4
-        lr_trunk = 1e-4
+        self.optuna = optuna
+        self.trial = optuna_trial
 
         # Optimizers
-        self.optim_branch = torch.optim.Adam(model.branch.parameters(), lr=lr_branch)
-        self.optim_trunk = torch.optim.Adam(model.trunk.parameters(), lr=lr_trunk)
+        if self.optuna:
+            self.optim = getattr(torch.optim, self.trial.params["optimizer"])(model.parameters(), lr=self.trial.params["lr"])
+        else:
+            lr = 1e-4
+            self.optim = torch.optim.AdamW(model.parameters(), lr=lr)
 
         # Schedulers
         if self.schedule:
+            if self.optuna:
+                if self.trial.params["scheduler"] == "InverseSquareRootLR":
+                    self.scheduler = InverseSquareRootLR(self.optim, warmup_steps=200, init_lr=self.trial.params["lr"], min_lr=self.trial.params["min_lr"])
+                else:
+                    scheduler_lambda = torch.optim.lr_scheduler.LambdaLR(self.optim, lr_lambda=lambda epoch: warmup_lr_lambda(epoch, 200))
+                    scheduler_step = torch.optim.lr_scheduler.StepLR(self.optim, step_size=50, gamma=0.99)
+                    self.scheduler = CombinedScheduler(self.optim, scheduler_lambda, scheduler_step, 200)
 
-            scheduler_lambda_branch = torch.optim.lr_scheduler.LambdaLR(self.optim_branch, lr_lambda=lambda epoch: warmup_lr_lambda(epoch, 200))
-            scheduler_step_branch = torch.optim.lr_scheduler.StepLR(self.optim_branch, step_size=50, gamma=0.99)
-            self.scheduler_branch = CombinedScheduler(self.optim_branch, scheduler_lambda_branch, scheduler_step_branch, 200)
-
-            scheduler_lambda_trunk = torch.optim.lr_scheduler.LambdaLR(self.optim_trunk, lr_lambda=lambda epoch: warmup_lr_lambda(epoch, 200))
-            scheduler_step_trunk = torch.optim.lr_scheduler.StepLR(self.optim_trunk, step_size=50, gamma=0.99)
-            self.scheduler_trunk = CombinedScheduler(self.optim_trunk, scheduler_lambda_trunk, scheduler_step_trunk, 200)
+            else:
+                scheduler_lambda = torch.optim.lr_scheduler.LambdaLR(self.optim, lr_lambda=lambda epoch: warmup_lr_lambda(epoch, 200))
+                scheduler_step = torch.optim.lr_scheduler.StepLR(self.optim, step_size=50, gamma=0.99)
+                self.scheduler = CombinedScheduler(self.optim, scheduler_lambda, scheduler_step, 200)
         
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
@@ -97,15 +130,12 @@ class Trainer:
             values, times = observations
             true_values, values, times = true_values.to(self.device), values.to(self.device), times.to(self.device)
 
-            # === Step 1: Forward pass for branch and trunk ===
-            branch_output = self.model.branch(values, times)
+            # === Forward pass for branch and trunk ===
 
-            trunk_output = self.model.trunk(self.model.fine_grid_points_batch.unsqueeze(-1))
-
-            final_output = torch.sum(trunk_output * branch_output.unsqueeze(-1), dim=-1)
+            out = self.model(values, times)
 
             # Compute loss
-            loss = self.criterion(final_output, true_values)
+            loss = self.criterion(out, true_values).item()
 
             cur_losses.append(loss)
             
@@ -120,38 +150,32 @@ class Trainer:
     def train_one_step(self, true_values, values, times):
         """ One training step """
         self.model.train()
-        # === Step 1: Forward pass for branch and trunk ===
-        branch_output = self.model.branch(values, times)
+        # === Forward pass for branch and trunk ===
 
-        trunk_output = self.model.trunk(self.model.fine_grid_points_batch.unsqueeze(-1))
-
-        final_output = torch.sum(trunk_output * branch_output.unsqueeze(-1), dim=-1)
+        out = self.model(values, times)
 
         # Compute loss
-        loss = self.criterion(final_output, true_values)
+        loss = self.criterion(out, true_values)
         
-        # === Step 2: Backward pass ===
+        # === Backward pass ===
 
-        self.optim_branch.zero_grad()
-        self.optim_trunk.zero_grad()
-
+        self.optim.zero_grad()
         loss.backward()
-
-        self.optim_branch.step()
-        self.optim_trunk.step()
+        self.optim.step()
 
         if self.schedule:
-            self.scheduler_branch.step()
-            self.scheduler_trunk.step()          
+            self.scheduler.step()
+
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)      
             
         return loss
 
     def fit(self):
         """ Train/Validation loop """
-        self.iter_ = self.inital_iter
+        self.iter_ = self.inital_epoch
         torch.autograd.set_detect_anomaly(True)
         
-        for ep in range(self.inital_iter, self.n_iters + self.inital_iter):
+        for ep in range(self.inital_epoch, self.epochs + self.inital_epoch):
             progress_bar = tqdm(enumerate(self.train_loader), total=(len(self.train_loader)), initial=0)
             for i, (true_values, observations) in progress_bar:     
                 # setting inputs to GPU
@@ -159,49 +183,61 @@ class Trainer:
                 true_values, values, times = true_values.to(self.device), values.to(self.device), times.to(self.device)
                 # forward pass and loss
                 mse_loss = self.train_one_step(true_values, values, times).item()
-
                 self.train_loss.append(mse_loss)
-            
-                # updating progress bar
-                progress_bar.set_description(f"Ep {ep} Iter {i+1}: Loss={round(mse_loss,5)}")
 
-                #adding loss to tensorboard
-                self.writer.add_scalars("Loss/Train", {
-                    'MSE': mse_loss,
-                }, self.iter_)
+                # only log progress if not optuna trial
+                if (not self.optuna):
+                    # updating progress bar
+                    progress_bar.set_description(f"Ep {ep} Iter {i+1}: Loss={round(mse_loss,5)}")
 
-                # adding lr to tensorboard
-                if self.schedule:
-                    self.writer.add_scalar("LR", self.scheduler_branch.get_last_lr()[0], self.iter_)
-                else:
-                    self.writer.add_scalar("LR", 1e-4, self.iter_)
+                    #adding loss to tensorboard
+                    self.writer.add_scalar("MSE/Train", mse_loss, self.iter_)
+                    self.writer.add_scalar("RMSE/Train", np.sqrt(mse_loss), self.iter_)
+
+                    # adding lr to tensorboard
+                    if self.schedule:
+                        self.writer.add_scalar("LR", self.scheduler.get_last_lr()[0], self.iter_)
+                    else:
+                        self.writer.add_scalar("LR", 1e-4, self.iter_)
                 
                 # doing some validation every once in a while
-                if(self.iter_ % 250 == 0):
+                if(self.iter_ % 50 == 0):
                     cur_losses = self.valid_step()
-                    print(f"Valid loss @ iteration {self.iter_}: Loss={np.mean(cur_losses)}")
+                    # Optuna pruning
+                    if self.optuna:
+                        self.trial.report(np.mean(cur_losses), step=self.iter_)
+                        if self.trial.should_prune():
+                            raise optuna.TrialPruned()
+                    else:
+                        print(f"Valid loss @ iteration {self.iter_}: Loss={np.mean(cur_losses)}")
+                        
                     # plot depth images
-                    self.writer.add_scalar("Loss/Valid", np.mean(cur_losses), self.iter_)
+                    if (not self.optuna): # only log into tensorboard and save if not optuna trial
+                        self.writer.add_scalar("MSE/Valid", np.mean(cur_losses), self.iter_)
+                        self.writer.add_scalar("RMSE/Valid", np.sqrt(np.mean(cur_losses)), self.iter_)
 
-                    # plot function predictions
-                    fig = plot_progress(self.valid_loader, self.model)
-                    self.writer.add_figure('Predictions', fig, global_step=self.iter_)
+                        # plot function predictions
+                        fig = plot_progress(self.valid_loader, self.model, self.device)
+                        self.writer.add_figure('Predictions', fig, global_step=self.iter_)
 
                     # save model
-                    stats = {
-                        "train_loss": self.train_loss,
-                        "valid_loss": self.valid_loss
-                    }
-                    optimizers = {
-                        "branch": self.optim_branch,
-                        "trunk": self.optim_trunk,
-                    }
-                    save_model(self.model, optimizers, self.iter_, stats, self.modelname)
+                        stats = {
+                            "train_loss": self.train_loss,
+                            "valid_loss": self.valid_loss
+                        }
+                        optimizers = {
+                            "model": self.optim,
+                            #"trunk": self.optim_trunk,
+                        }
+                        save_model(self.model, optimizers, self.iter_, stats, self.modelname)
                 
                 self.iter_ = self.iter_+1 
-                if(self.iter_ >= (self.n_iters - self.inital_iter)*len(self.train_loader)):
+                if(self.iter_ >= (self.epochs - self.inital_epoch)*len(self.train_loader)):
                     break
-            if(self.iter_ >= (self.n_iters - self.inital_iter)*len(self.train_loader)):
+            if(self.iter_ >= (self.epochs - self.inital_epoch)*len(self.train_loader)):
                 break
-            
+        
+        if optuna:
+            return np.mean(self.valid_loss)
+        
         return
